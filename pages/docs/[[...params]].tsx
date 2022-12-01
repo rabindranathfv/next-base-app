6a577168f29ff2bf1ecec09638bad7ac4a8e6418
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-12-01 17:14:12 +0100

    dynamic routes with with params routes as array with catch AllRoutes with [[ ]] for dynamics views

---
+++ pages/docs/[[...params]].tsx
@@ -0,0 +1,32 @@
+import { useRouter } from "next/router";
+
+// route exampel as /docs/feature<1....n>/concepts<1....n>
+const Docs = () => {
+  const router = useRouter();
+  const { params = [] } = router.query;
+  console.log("🚀 ~ file: [...params].tsx:6 ~ Docs ~ query", params);
+
+  if (params.length === 2) {
+    return (
+      <div>
+        <h2>
+          view docs feature {params[0]} and concept {params[1]}
+        </h2>
+      </div>
+    );
+  } else if (params.length === 1) {
+    return (
+      <div>
+        <h2>view docs feature {params[0]}</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Docs VIEW</h1>
+    </div>
+  );
+};
+
+export default Docs;

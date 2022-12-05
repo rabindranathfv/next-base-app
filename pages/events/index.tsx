aed3be6734a7761ad8c17024086b3f7d6f85c736
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-12-05 21:18:33 +0100

    combine client side redering with pre-rederer ssr

---
+++ pages/events/index.tsx
@@ -0,0 +1,67 @@
+import { useRouter } from "next/router";
+import { GetServerSideProps, GetServerSidePropsContext } from "next/types";
+import React, { useState } from "react";
+import { UrlWithParsedQuery } from "url";
+
+interface Event {
+  id: Number;
+  title: String;
+  description: String;
+  category: String;
+  Date: String;
+}
+
+interface IEventList {
+  eventList: Event[];
+}
+
+const EventList = ({ eventList }: IEventList) => {
+  const [eventListFiltered, setEventListFiltered] = useState(eventList);
+  const router = useRouter();
+
+  const fetchSportEvents = async () => {
+    const fetchUrl = `http://localhost:4000/events?category=sports`;
+    const resp = await fetch(fetchUrl);
+    const data = await resp.json();
+
+    setEventListFiltered(data);
+    router.push(`/events?category=sports`, undefined, { shallow: true });
+  };
+
+  return (
+    <div>
+      <h1>Event list</h1>
+      <button onClick={fetchSportEvents}>Filter by Category</button>
+
+      {eventListFiltered.map((event: Event) => {
+        return (
+          <div key={event.id.toString()}>
+            <h2>
+              {event.id} {event.title} {event.Date} | {event.category}
+            </h2>
+            <p>{event.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default EventList;
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { query } = context;
+  const queryStr = query?.category ? `category=sports` : ``;
+  const fetchUrl = `http://localhost:4000/events?${queryStr}`;
+  const resp = await fetch(fetchUrl);
+  const data = await resp.json();
+
+  return {
+    props: {
+      eventList: data,
+    },
+  };
+};

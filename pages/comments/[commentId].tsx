8687941c50ba303572ddc30123ec14f34b559e80
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-12-07 12:32:21 +0100

    API next with pre rendering getStaticProps and getStaticPaths

--- pages/comments/[commentId].tsx
+++ pages/comments/[commentId].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { GetServerSidePropsContext } from "next/types";
 import React, { useEffect, useState } from "react";
 
 interface Comment {
@@ -6,7 +7,9 @@ interface Comment {
   text: string;
 }
 
-const CommentDetail = () => {
+const CommentDetail = (props: any) => {
+  console.log("🚀 ~ file: [commentId].tsx:13 ~ CommentDetail ~ props", props);
+
   const router = useRouter();
   const { commentId } = router.query;
 
@@ -27,9 +30,41 @@ const CommentDetail = () => {
     <div>
       <h1>CommentDetail of {commentId}</h1>
 
-      {Object.keys(comment).length > 0 && <p>{comment.text}</p>}
+      <p>
+        {comment.id?.toString()} - {comment.text}
+      </p>
     </div>
   );
 };
 
 export default CommentDetail;
+
+export async function getStaticPaths() {
+  const res = await fetch(`http://localhost:3000/api/comments/`);
+  const data = await res.json();
+
+  const paths = data.map((c: Comment) => ({
+    params: { commentId: c.id.toString() },
+  }));
+
+  return {
+    paths,
+    // fallback: true make request on build time, generate new static files with those request and you can use router with isFallback property if needed for give UI Feedback,
+    // fallback: blocking request on build time, generate new static files but doest have any connection with UI Interface
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context: GetServerSidePropsContext) {
+  const { params } = context;
+  const res = await fetch(
+    `http://localhost:3000/api/comments/${params?.commentId}`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      comment: data,
+    },
+  };
+}
